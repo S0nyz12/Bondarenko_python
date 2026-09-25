7da8026ac3f0fc5fa57ce7dd4e3412a2c5e50ddf
S0nyz12 <sonchka1290@mail.ru>
2026-09-25 12:50:04 +0300

    Add files via upload

---
+++ занятие1.py
@@ -0,0 +1,191 @@
+#begin1
+a = float(input())
+perimeter = 4 * a
+print(perimeter)
+
+#begin2
+a = float(input()) 
+area = a ** 2 
+print(area)
+
+#begin3
+a = float(input()) 
+b = float(input())
+
+area = a * b 
+perimeter = 2 * (a + b) 
+
+print(area) 
+print(perimeter)
+
+#begin4
+d = float(input()) 
+
+pi = 3.14 
+length = pi * d 
+
+print(length)
+
+#begin5
+a = float(input())
+
+volume = a ** 3
+surface = 6 * a ** 2
+
+print(volume)
+print(surface)
+
+#begin6
+a = float(input()) 
+b = float(input()) 
+c = float(input()) 
+
+volume = a * b * c 
+surface = 2 * (a * b + b * c + a * c) 
+
+print(volume) 
+print(surface)
+
+#begin7
+r = float(input()) 
+pi = 3.14 
+length = 2 * pi * r 
+area = pi * r ** 2 
+
+print(length) 
+print(area)
+
+#begin8
+a = float(input()) 
+b = float(input()) 
+average = (a + b) / 2 
+print(average)
+
+#begin9
+a = float(input()) 
+b = float(input()) 
+geometric_mean = (a * b) ** 0.5 
+print(geometric_mean)
+
+#begin10
+a = float(input()) 
+b = float(input())
+
+a_square = a ** 2 
+b_square = b ** 2 
+
+print(a_square + b_square) 
+print(a_square - b_square) 
+print(a_square * b_square) 
+print(a_square / b_square)
+
+#begin11
+a = float(input()) 
+b = float(input()) 
+
+a_abs = abs(a) 
+b_abs = abs(b) 
+
+print(a_abs + b_abs) 
+print(a_abs - b_abs) 
+print(a_abs * b_abs) 
+print(a_abs / b_abs)
+
+#begin12
+a = float(input()) 
+b = float(input()) 
+c = (a ** 2 + b ** 2) ** 0.5 
+
+perimeter = a + b + c 
+
+print(c) 
+print(perimeter)
+
+#begin13
+r1 = float(input()) 
+r2 = float(input()) 
+pi = 3.14 
+
+s1 = pi * r1 ** 2 
+s2 = pi * r2 ** 2 
+s3 = s1 - s2 
+
+print(s1) 
+print(s2) 
+print(s3)
+
+#begin14
+length = float(input()) 
+pi = 3.14 
+r = length / (2 * pi) 
+area = pi * r ** 2 
+
+print(r) 
+print(area)
+
+#begin15
+area = float(input())
+pi = 3.14
+
+r = (area / pi) ** 0.5
+diameter = 2 * r
+length = 2 * pi * r
+
+print(diameter)
+print(length)
+
+#begin16
+x1 = float(input()) 
+x2 = float(input()) 
+distance = abs(x2 - x1) 
+
+print(distance)
+
+#begin17
+a = float(input()) 
+b = float(input()) 
+c = float(input()) 
+
+ac = abs(c - a) 
+bc = abs(c - b) 
+total = ac + bc 
+
+print(ac) 
+print(bc) 
+print(total)
+
+#begin18
+a = float(input()) 
+b = float(input()) 
+c = float(input()) 
+
+ac = abs(c - a) 
+bc = abs(c - b) 
+product = ac * bc 
+
+print(product)
+
+#begin19
+x1 = float(input()) 
+y1 = float(input()) 
+
+x2 = float(input()) 
+y2 = float(input()) 
+
+width = abs(x2 - x1) 
+height = abs(y2 - y1) 
+perimeter = 2 * (width + height) 
+area = width * height 
+
+print(perimeter) 
+print(area)
+
+#begin20
+x1 = float(input()) 
+y1 = float(input()) 
+
+x2 = float(input()) 
+y2 = float(input()) 
+
+distance = ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5 
+print(distance)
